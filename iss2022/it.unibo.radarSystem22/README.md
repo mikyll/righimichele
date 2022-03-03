c29c93bed150119b101dfc52cb44292936d335c7
Michele Righi <righi.michy@gmail.com>
2022-03-04 00:59:30 +0100

    radarSystem22

---
+++ iss2022/it.unibo.radarSystem22/README.md
@@ -0,0 +1,9 @@
+
+
+- impostare la versione del JDK del progetto:
+	1. Properties (Click destro sul progetto)
+	2. Build Path
+	3. Libraries
+	4. JRE System Library [...] > Edit
+	5. Execution environment: JavaSE-1.8 (jdk1.8.0_281)
+		*foto*
--- iss2022/it.unibo.radarSystem22/src/it/unibo/radarSystem22/main/RadarUsageMain.java
+++ iss2022/it.unibo.radarSystem22/src/it/unibo/radarSystem22/main/RadarUsageMain.java
@@ -1,14 +1,13 @@
 package it.unibo.radarSystem22.main;
-
 import radarPojo.radarSupport;
 
 public class RadarUsageMain {
- 	public void doJob() {
-		System.out.println("start");
-		radarSupport.setUpRadarGui();
- 		radarSupport.update( "40", "60");
- 	}
-	public static void main(String[] args) {
-		new RadarUsageMain().doJob();
-	}
+    public void doJob() {
+        System.out.println("start");
+        radarSupport.setUpRadarGui();
+        radarSupport.update( "40", "60");
+    }
+    public static void main(String[] args) {
+        new RadarUsageMain().doJob();
+    }
 }
