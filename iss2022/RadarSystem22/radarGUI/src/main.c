ccf45acd745bf3f5ed28eb6c8753e6f1195a245a
Michele Righi <righi.michy@gmail.com>
2022-04-20 23:37:47 +0200

    Update main.c

--- iss2022/RadarSystem22/radarGUI/src/main.c
+++ iss2022/RadarSystem22/radarGUI/src/main.c
@@ -1,25 +1,23 @@
 #include "main.h"
 
+static void doArguments(int argc, char** argv);
 static void capFrameRate(long* then, float* remainder);
 
-static int t = 1000 / FPS;
+static long t = 1000 / FPS;
 static int r;
 
 int main(int argc, char** argv)
 {
 	long then;
 	float remainder;
-
-	r = (1000 / FPS) - (float)t;
-
-	int i, j, k;
 	Uint64 start, end;
 	float elapsed;
 
 	memset(&app, 0, sizeof(App));
 
-	if (argc == 2 && strcmp(argv[1], "-d") == 0)
-		app.debug = 1;
+	doArguments(argc, argv);
+
+	r = (1000 / FPS) - (float)t;
 
 	srand(time(NULL));
 
@@ -62,6 +60,60 @@ int main(int argc, char** argv)
 	}
 
 	return 0;
+}
+
+static void doArguments(int argc, char** argv)
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (STRCMP(argv[i], == , "-d") || STRCMP(argv[i], == , "--debug"))
+		{
+			app.args.flags[FLAG_DEBUG] = 1;
+			app.args.debug = 1;
+		}
+		else if (STRCMP(argv[i], == , "-s") || STRCMP(argv[i], == , "--sounds"))
+		{
+			app.args.flags[FLAG_SOUNDS] = 1;
+			app.args.sounds = 1;
+		}
+		else if (STRCMP(argv[i], == , "--tcp"))
+		{
+			app.args.flags[FLAG_TCP_SERVER_PORT] = 1;
+			i++;
+			app.args.TCPport = atoi(argv[i]);
+			if (app.args.TCPport < 1024 || app.args.TCPport > 65535 || app.args.TCPport == DEFAULT_UDP_PORT)
+			{
+				// use default port
+				app.args.TCPport = DEFAULT_TCP_SERVER_PORT;
+			}
+		}
+		else if (STRCMP(argv[i], == , "--udp"))
+		{
+			app.args.flags[FLAG_UDP_PORT] = 1;
+			i++;
+			app.args.UDPport = atoi(argv[i]);
+			if (app.args.UDPport < 1024 || app.args.UDPport > 65535 || app.args.UDPport == DEFAULT_TCP_SERVER_PORT)
+			{
+				// use default port
+				app.args.UDPport = DEFAULT_UDP_PORT;
+			}
+		}
+		else
+		{
+			app.args.flags[FLAG_ERR] = 1;
+			break;
+		}
+	}
+
+	// test
+	printf("app.args.flags[FLAG_DEBUG]=%d - app.args.debug=%d\n", app.args.flags[FLAG_DEBUG], app.args.debug);
+
+	if (app.args.flags[FLAG_ERR])
+	{
+		// ignore(?)
+	}
 }
 
 static void capFrameRate(long* then, float* remainder)
